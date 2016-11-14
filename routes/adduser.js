531e1afb273abc47bc998cea7cb56445b2792838
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-11-14 17:09:06 +1300

    added many mongodb methods to project

--- routes/adduser.js
+++ routes/adduser.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var twitter = require('../lib/twitter1.js');
+var mongodb = require('../lib/mongodb.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
   twitter.getProfile(req.query.id, (profile_data) => {
+    mongodb.add_twitter_account(profile_data);
     res.send(profile_data);
   })
 });
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,16 @@ var router = express.Router();
 var twitter = require('../lib/twitter1');
 var facebook = require('../lib/facebook');
 var twitterkeys = require('../lib/twitterkeys');
+var mongo = require('../lib/mongodb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  twitter.tom((x) => {
-    x.forEach(function(element) {
-      console.log(element.text);
-    }, this);
-  })
-  console.log(req);
+  // twitter.tom((x) => {
+  //   x.forEach(function(element) {
+  //     console.log(element.text);
+  //   }, this);
+  // })
+  mongo.test();
   res.render('index', { title: twitterkeys.consumerSecret });
 });
 
